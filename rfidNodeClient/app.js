b3c0bff04c86a50e571500b19ceeda43d68695c9
Martin Holt Juliussen <martinhj@gmail.com>
2014-08-01 11:04:10 +0200

    Now the rfid client joins the server and simulates a goggle read.

--- rfidNodeClient/app.js
+++ rfidNodeClient/app.js
@@ -1,9 +1,35 @@
 var io = require('socket.io-client')('http://localhost:8080');
 var socket = io.connect();
 
-socket.on('connect', function() {
+socket.on('connect', function(data) {
 
-}
+      var nickname = {
+        name: '',
+        id: 0
+        };
+
+      nickname.name = 'a';
+      nickname.id = 3;
+
+      
+      socket.emit('join', nickname);
+
+
+      var readObject = {
+        kaiId: 0,
+        kaiRead: 'aaa1'
+      };
+      socket.emit('read', readObject);
+
+
+
+      });
+
+
+
+  socket.on('messages', function (data) {
+      console.log(data.hello);
+      });
 /*
 socket.on('connect', function() {
   socket.on('event', function(data) {});
