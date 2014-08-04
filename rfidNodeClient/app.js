9619ff2778c3436df9045af1c3f360464444fc02
Martin Holt Juliussen <martinhj@gmail.com>
2014-08-04 14:01:55 +0200

    Fixed object printout. Fixed console printuot. Some cleanup.

--- rfidNodeClient/app.js
+++ rfidNodeClient/app.js
@@ -3,72 +3,84 @@ var socket = io.connect();
 
 
 
+// set information to read keyboard input
 var readline = require('readline');
-
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+// set the term Prompt
+rl.setPrompt('kaiTERM> ');
 
 
 
-
-
+// connect event.
 socket.on('connect', function(data) {
 
 
 
-  rl.question("What goggle do you want to test? ", function (answer) {
-    var answerTest = {};
-    answerTest.answer = parseInt(answer, 10);
-    console.log(answerTest);
-    rl.close();
-  });
+  var nickname = {
+    name: 'paranoidAndroid',
+    id: 3
+  };
 
 
-      var nickname = {
-        name: '',
-        id: 0
-        };
-
-      nickname.name = 'a';
-      nickname.id = 3;
-
-      
-      socket.emit('join', nickname);
-
-
-      var readObject = {
-        kaiId: 0,
-        kaiRead: 'aaa1'
-      };
-      socket.emit('read', readObject);
-
-
-      readObject = {
-        kaiId: 1,
-        kaiRead: 'aba1'
-      };
-      socket.emit('read', readObject);
-  
-
-      readObject = {
-        kaiId: 0,
-        kaiRead: 'bbb1'
-      };
-      socket.emit('read', readObject);
-
-
-      });
+  socket.emit('join', nickname);
 
 
 
-  socket.on('messages', function (data) {
-      console.log(data.hello);
-      });
-/*
-socket.on('connect', function() {
-  socket.on('event', function(data) {});
-  socket.on('disconnect', function() {});
+}); // end connect
+
+
+
+/**
+ * What to do with a message
+ */
+socket.on('messages', function (data) {
+  console.log("\n> " + data.hello);
+  rl.prompt();
 });
-*/
+
+
+
+/**
+ * Different objects to send to the server.
+ * kaiId is the 'tablets' id, the rfid reader would be configured with the
+ * one corresponding to the one on the tablet.
+ */
+readObjects = [];
+readObjects.push({
+  kaiId: 0,
+  kaiRead: 0
+});
+readObjects.push({
+  kaiId: 0,
+  kaiRead: 1
+});
+
+
+
+/**
+ * When sending a line from the keyboard to the app.
+ */
+rl.on('line', function(line) {
+  switch(line.trim()) {
+    case '0':
+      socket.emit('read', readObjects[0]);
+      break;
+    case '1':
+      socket.emit('read', readObjects[1]);
+      break;
+    default:
+      whichObjects();
+  }
+  rl.prompt();
+});
+
+
+function whichObjects() {
+  console.log('Simulate different reads and send it to the server.');
+  for (var i = 0, l = readObjects.length; i < l; i++) {
+    console.log('Press \'' + i + '\' for ' + JSON.stringify(readObjects[i]));
+  }
+}
