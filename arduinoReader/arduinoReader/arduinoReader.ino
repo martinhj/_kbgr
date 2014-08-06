96af436e219a1a86cd33a36f66ff335612ecc8e6
Martin Holt Juliussen <martinhj@gmail.com>
2014-08-06 13:36:50 +0200

    Started to work with the setup to connect to the rfid hardware.

--- arduinoReader/arduinoReader/arduinoReader.ino
+++ arduinoReader/arduinoReader/arduinoReader.ino
@@ -1,1 +1,34 @@
+#include <Wire.h>
+#include <Adafruit_NFCShield_I2C.h>
 
+#define IRQ   (2)
+#define RESET (3)  // Not connected by default on the NFC Shield
+
+Adafruit_NFCShield_I2C nfc(IRQ, RESET);
+
+void setup(void) {
+  Serial.begin(115200);
+  Serial.println("kaiHello!");
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+  
+  Serial.println("Waiting for an ISO14443A Card ...");
+}
+
+void loop(void) {
+  Serial.println("This is a kaiTest message");
+  delay(10000);
+}
