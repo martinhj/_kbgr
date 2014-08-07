d2daddb0ac7679d5afb52cf8f00f3d66ae28c3fe
Martin Holt Juliussen <martinhj@gmail.com>
2014-08-07 12:27:48 +0200

    Now it outputs only the first char of block 4 unless it's any errors.

--- arduinoReader/arduinoReader.ino
+++ arduinoReader/arduinoReader.ino
@@ -18,7 +18,6 @@ Adafruit_NFCShield_I2C nfc(IRQ, RESET);
 
 void setup(void) {
   Serial.begin(115200);
-  Serial.println("kaiHello!");
 
   nfc.begin();
 
@@ -28,17 +27,80 @@ void setup(void) {
     while (1); // halt
   }
   // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
   // configure board to read RFID tags
   nfc.SAMConfig();
   
-  Serial.println("Waiting for an ISO14443A Card ...");
+  /*Serial.println("Waiting for an ISO14443A Card ...");*/
 }
 
+
+
+uint8_t lastRead;
+
+
+
 void loop(void) {
-  Serial.println("This is a kaiTest message");
-  delay(10000);
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  uint8_t block = 4;
+
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  
+  if (success) {
+    // Display some basic information about the card
+    /*nfc.PrintHex(uid, uidLength);*/
+    
+    if (uidLength == 4)
+    {
+      // Now we need to try to authenticate it for read/write access
+      // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
+      uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+    
+      // Start with block 4 (the first block of sector 1) since sector 0
+      // contains the manufacturer data and it's probably better just
+      // to leave it alone unless you know what you're doing
+      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
+    
+      if (success)
+      {
+        uint8_t data[16];
+    
+
+
+
+        // Try to read the contents of block 4
+        success = nfc.mifareclassic_ReadDataBlock(block, data);
+    
+        if (success)
+        {
+          // Data seems to have been read ... spit it out
+
+          if (data[0] != lastRead) {
+            lastRead = data[0];
+            Serial.println(data[0]);
+          } //else {
+            //Serial.println("Same as last time!");
+          //}
+          /*nfc.PrintHexChar(data, 16);*/
+      
+          // Wait a bit before reading the card again
+          delay(1000);
+        }
+        else
+        {
+          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
+        }
+      }
+      else
+      {
+        Serial.println("Ooops ... authentication failed: Try another key?");
+      }
+    }
+    
+  }
 }
+
